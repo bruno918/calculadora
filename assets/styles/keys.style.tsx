824fffad528cea38bd4e85fba8339247fd6044e1
Brunoww <82423722+bruno918@users.noreply.github.com>
2022-10-28 22:23:55 -0300

    added KeyText style

--- assets/styles/keys.style.tsx
+++ assets/styles/keys.style.tsx
@@ -1,35 +1,38 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const keysStyles = StyleSheet.create({
   rowKeys: {
     paddingTop: 10,
     paddingRight: 10,
-    display: 'flex',
-    flexDirection: 'row'
+    display: "flex",
+    flexDirection: "row",
+  },
+  keyText: {
+    color: '#000',
   },
   columnKey: {
-    width: '22.2%',
+    width: "22.2%",
     height: 55,
     marginLeft: 10,
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 19,
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 6,
     shadowOffset: {
       width: 0,
-      height: 3
+      height: 3,
     },
     shadowRadius: 6,
-    shadowColor: '#c9c9c9'
+    shadowColor: "#c9c9c9",
   },
-  keyText: {
-    fontFamily: 'Arial',
-    fontSize: 19
+  text: {
+    fontSize: 19,
+    fontFamily: "Arial",
   },
   backspace: {
     width: 30,
-    height: 30
-  }
+    height: 30,
+  },
 });
